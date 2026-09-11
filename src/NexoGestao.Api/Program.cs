413f9a1c4c53fb2107172505c5ba5e52311e6d35
Josiana Cruz <josiana.cruz1@gmail.com>
2026-09-10 22:43:01 -0300

    Conecta frontend ao backend via CORS (teste com weatherforecast)

--- src/NexoGestao.Api/Program.cs
+++ src/NexoGestao.Api/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFrontendDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("PermitirFrontendDev");
 
 app.UseAuthorization();
 
